b7fb53ed1d94d8d6b965a82c0d7f524f0965f4bb
Vincent Daubry <vdaubry@gmail.com>
2025-04-16 21:05:33 +0200

    move extensions OpenFresk

--- lib/open_fresk/engine.rb
+++ lib/open_fresk/engine.rb
@@ -13,12 +13,19 @@ module OpenFresk
 
     initializer "open_fresk.add_autoload_paths", before: :set_autoload_paths do |app|
       # Reassign autoload and eager load paths rather than mutating a frozen array.
-      config.autoload_paths = config.autoload_paths + [root.join("app", "classes")]
-      config.eager_load_paths = config.eager_load_paths + [root.join("app", "classes")]
+      config.autoload_paths = config.autoload_paths + [root.join("app", "services")]
+      config.eager_load_paths = config.eager_load_paths + [root.join("app", "services")]
     end
 
     initializer "open_fresk.assets.precompile" do |app|
       app.config.assets.precompile += %w( open_fresk/application.css )
+    end
+
+    # Load core_extensions before the application initializes
+    initializer "open_fresk.core_ext.string", before: :load_config_initializers do
+      config.to_prepare do
+        ::String.include OpenFresk::CoreExtensions::String
+      end
     end
   end
 end
