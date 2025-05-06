6624b8e8701a1857129eb53c8dee0505dea4730d
Vincent Daubry <vdaubry@gmail.com>
2025-05-06 17:58:46 +0200

    chore/authent-session

--- app/controllers/open_fresk/sessions_controller.rb
+++ app/controllers/open_fresk/sessions_controller.rb
@@ -3,6 +3,34 @@ module OpenFresk
     def new
     end
 
+    def create
+      user = ::User.where(email: params[:email]&.downcase).first
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
 
+        # Allow host app to add custom behavior
+        after_sign_in_hook(user)
+
+        redirect_to redirection_url(user)
+      else
+        @alert = t("sessions.invalid_credentials")
+        respond_to do |format|
+          format.html { redirect_to open_fresk.new_session_path, alert: @alert }
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.prepend("signin", partial: "wrong_credentials", locals: {alert: @alert})
+          end
+        end
+      end
+    end
+
+    private
+
+      def redirection_url(user)
+        root_url
+      end
+
+      def after_sign_in_hook(user)
+        # No-op by default
+      end
   end
 end
