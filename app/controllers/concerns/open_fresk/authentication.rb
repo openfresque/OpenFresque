acdf20da73468149ef5fd5e592945338939c64dd
Vincent Daubry <vdaubry@gmail.com>
2025-05-07 21:23:10 +0200

    Refactor inheritance [STABLE]

---
+++ app/controllers/concerns/open_fresk/authentication.rb
@@ -0,0 +1,18 @@
+module OpenFresk::Authentication
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :current_user
+    before_action  :authenticate_user!
+  end
+
+  def current_user
+    @current_user ||= ::User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user!
+    return if current_user
+    session[:forwarding_url] = request.fullpath if request.get?
+    redirect_to open_fresk.new_session_path, alert: t("please_log_in")
+  end
+end
--- app/controllers/open_fresk/application_controller.rb
+++ app/controllers/open_fresk/application_controller.rb
@@ -1,19 +1,5 @@
 module OpenFresk
-  class ApplicationController < ActionController::Base
-    helper_method :current_user
-
-    before_action :authenticate_user!
-    def current_user
-      @current_user ||= ::User.where(id: session[:user_id]).first
-    end
-
-    def authenticate_user!
-      return unless current_user.nil?
-
-      session[:forwarding_url] = request.original_url if request.get?
-      session[:user_id] = nil
-      redirect_to open_fresk.new_session_path(language: language || "fr"), status: 301
-    end
-
+  class ApplicationController < ::ApplicationController
+    include OpenFresk::Authentication
   end
 end
