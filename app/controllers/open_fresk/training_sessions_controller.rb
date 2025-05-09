769e40ac7f5cbe7f0dfd8a46c4f85cb1319620a1
Vincent Daubry <vdaubry@gmail.com>
2025-05-09 14:59:45 +0200

    refactor my_sessions list

--- app/controllers/open_fresk/training_sessions_controller.rb
+++ app/controllers/open_fresk/training_sessions_controller.rb
@@ -2,5 +2,16 @@ module OpenFresk
   class TrainingSessionsController < ApplicationController
     def index
     end
+
+    private
+
+      def sessions_lists
+        public_opportunities
+      end
+
+      def public_opportunities
+        training_sessions = TrainingSession.futur
+        @my_training_sessions = training_sessions.my_sessions(current_user)
+      end
   end
 end
