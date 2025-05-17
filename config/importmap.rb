af508c724a38b4ad7557ee65bfb259e15ab133d7
Vincent Daubry <vdaubry@gmail.com>
2025-05-17 15:29:29 +0200

    add recover password stimulus controller

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
 pin "open_fresk", to: "open_fresk/application.js", preload: true
 pin_all_from File.expand_path("../app/javascript/open_fresk/controllers", __dir__),
              under: "open_fresk/controllers"
+pin "open_fresk/recover_password",
+             to: "open_fresk/recover_password.js",
+             preload: true
