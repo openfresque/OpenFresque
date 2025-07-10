1853784358049bd85ca2c78ffd562793ec67efa5
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-07-10 12:07:33 +0200

    add method to select a language and load corresponding traductions

--- app/controllers/open_fresk/application_controller.rb
+++ app/controllers/open_fresk/application_controller.rb
@@ -1,5 +1,23 @@
 module OpenFresk
   class ApplicationController < ::ApplicationController
     include OpenFresk::Authentication
+
+    before_action :set_unsigned_language
+
+    def language
+      params[:language]
+    end
+
+    def set_unsigned_language
+      return unless language
+
+      if %w[fr en].include?(language)
+        I18n.locale = language
+      elsif language.include?('\u0026')
+        redirect_to request.original_url.gsub('\\u0026', '&')
+      else
+        redirect_to request.original_url.gsub("language=#{language}", 'language=en')
+      end
+    end
   end
 end
