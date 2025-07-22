351449f879439757cf979f9cd5112b0b845c0a1b
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-07-22 17:51:04 +0200

    add save language

--- app/controllers/open_fresk/application_controller.rb
+++ app/controllers/open_fresk/application_controller.rb
@@ -16,8 +16,18 @@ module OpenFresk
       elsif language.include?('\u0026')
         redirect_to request.original_url.gsub('\\u0026', '&')
       else
-        redirect_to request.original_url.gsub("language=#{language}", 'language=en')
+        redirect_to request.original_url.gsub('language=en')
       end
+    end
+
+    def set_signed_language
+      return unless current_user
+
+      if language && (current_user.native_language.nil? || current_user.native_language != language)
+        current_user.update(native_language: language)
+      end
+
+      I18n.locale = current_user.native_language
     end
   end
 end
