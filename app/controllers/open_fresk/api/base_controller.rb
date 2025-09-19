7143cc81fe9b2823fe9c4419c84de74fe1e6d353
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-09-19 16:02:10 +0200

    add internal server errors

--- app/controllers/open_fresk/api/base_controller.rb
+++ app/controllers/open_fresk/api/base_controller.rb
@@ -3,6 +3,7 @@ module OpenFresk
     class BaseController < ActionController::API
       rescue_from ActiveRecord::RecordNotFound, with: :not_found
       rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+      rescue_from StandardError, with: :internal_server_error
 
       private
 
@@ -12,6 +13,10 @@ module OpenFresk
 
       def unprocessable_entity(error)
         render json: { error: error.record.errors.full_messages }, status: :unprocessable_entity
+      end
+
+      def internal_server_error(error)
+        render json: { error: error.message }, status: :internal_server_error
       end
     end
   end
