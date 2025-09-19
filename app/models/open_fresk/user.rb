17b4a05f996735ff70e71a4be8b9e1feb66ba083
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-09-19 11:17:20 +0200

    add string_enum for user_role

--- app/models/open_fresk/user.rb
+++ app/models/open_fresk/user.rb
@@ -1,9 +1,18 @@
 module OpenFresk
   class User < ApplicationRecord
+    USER_ROLES = %w[user facilitator advanced_facilitator].freeze
+
     self.table_name = 'users'
     has_secure_password
 
     has_many :participations, dependent: :destroy
+
+    validates :user_role, presence: true, inclusion: { in: USER_ROLES }
+
+    USER_ROLES.each do |role|
+      define_method("#{role}?") { user_role == role }
+      scope role, -> { where(user_role: role) }
+    end
 
     def country
       Country.find_by(name: 'France')
