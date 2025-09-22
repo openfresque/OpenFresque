9b09dadf178f6b0b9c791f3557694fada993c10e
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-09-22 22:04:51 +0200

    add token api logic

--- app/controllers/open_fresk/api/base_controller.rb
+++ app/controllers/open_fresk/api/base_controller.rb
@@ -1,11 +1,20 @@
 module OpenFresk
   module Api
     class BaseController < ActionController::API
+      before_action :authenticate_with_token!
+
       rescue_from ActiveRecord::RecordNotFound, with: :not_found
       rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
       rescue_from StandardError, with: :internal_server_error
 
       private
+
+      def authenticate_with_token!
+        token = request.headers['Authorization']&.split(' ')&.last
+        api_token = ApiToken.find_by(token: token)
+
+        render json: { error: 'Unauthorized' }, status: :unauthorized if api_token.nil?
+      end
 
       def not_found(error)
         render json: { error: error.message }, status: :not_found
