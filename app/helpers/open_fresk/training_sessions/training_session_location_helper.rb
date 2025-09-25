b3cb70a06a8f7d6f1a9b548f5ca69e3059531ea0
Bastien Grandjean <Bastien@MacBook-Pro-de-Thibaut.local>
2025-09-25 17:39:03 +0200

    add online sessions

--- app/helpers/open_fresk/training_sessions/training_session_location_helper.rb
+++ app/helpers/open_fresk/training_sessions/training_session_location_helper.rb
@@ -11,13 +11,11 @@ module OpenFresk
 
       def training_session_online(session)
         icon = fa_icon('video', class: 'text-secondary')
-        text = if session.facilitator?(current_user) || session.observer?(current_user)
-                 link = link_to(t('training_sessions.visio_link'), @training_session.connexion_url,
-                                class: 'link-secondary text-decoration-none', target: :_blank)
-                 copy_btn = button_tag fa_icon('copy', type: :regular, text: t('copy')),
-                                       data: { text: @training_session.connexion_url }, class: 'toast_btn btn p-0 copy-to-clipboard link-secondary'
-                 content_tag :span, "(#{link} - #{copy_btn})".html_safe, class: 'link-secondary'
-               end
+        text = link = link_to(t('training_sessions.visio_link'), @training_session.connexion_url,
+                              class: 'link-secondary text-decoration-none', target: :_blank)
+        copy_btn = button_tag fa_icon('copy', type: :regular, text: t('copy')),
+                              data: { text: @training_session.connexion_url }, class: 'toast_btn btn p-0 copy-to-clipboard link-secondary'
+        content_tag :span, "(#{link} - #{copy_btn})".html_safe, class: 'link-secondary'
         content_tag(:div, "#{icon} #{t('training_sessions.online')} #{text}".html_safe)
       end
 
